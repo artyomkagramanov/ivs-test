8eabbe3fe4f1897b185d0ea7ae4d29cf5ab5370c
artyomkagramanov <artyomkagramanov@gmail.com>
2022-09-13 12:59:53 +0400

    changes

---
+++ src/helpers.js
@@ -0,0 +1,31 @@
+async function handlePermissions() {
+    let permissions = {
+        audio: false,
+        video: false,
+    };
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        for (const track of stream.getTracks()) {
+            track.stop();
+        }
+        permissions = { video: true, audio: true };
+    } catch (err) {
+        permissions = { video: false, audio: false, error: err.message };
+    }
+    return permissions;
+}
+
+async function getDevices() {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const videoDevices = devices.filter((d) => d.kind === 'videoinput');
+    const audioDevices = devices.filter((d) => d.kind === 'audioinput');
+    return {
+        videoDevices,
+        audioDevices
+    }
+}
+
+export {
+    handlePermissions,
+    getDevices,
+};
--- src/index.js
+++ src/index.js
@@ -2,27 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
-import IVSBroadcastClient, {
-  Errors,
-  BASIC_LANDSCAPE
-} from 'amazon-ivs-web-broadcast';
 
-const ingestServer = '';
-const streamKey = '';
-const client = IVSBroadcastClient.create({
-  // Enter the desired stream configuration
-  streamConfig: IVSBroadcastClient.BASIC_LANDSCAPE,
-  // Enter the ingest endpoint from the AWS console or CreateChannel API
-  ingestEndpoint: ingestServer,
-});
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log('steee')
-// root.render(
-//   <App />
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
