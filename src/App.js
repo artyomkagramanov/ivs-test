a05fecf6f75244563c3c43be7311d1d7be8b4f53
artyomkagramanov <artyomkagramanov@gmail.com>
2022-09-15 18:02:12 +0400

    permissions

--- src/App.js
+++ src/App.js
@@ -8,20 +8,28 @@ import IVSBroadcastClient, {
 } from 'amazon-ivs-web-broadcast';
 
 const streamConfig = BASIC_LANDSCAPE;
-
-const ingestServer = process.env.INGEST_SERVER;
+const streamKey = process.env.REACT_APP_STREAM_KEY;
+const ingestServer = process.env.REACT_APP_INGEST_SERVER;
 
 
 function App() {
   const client = useRef();
   const mainPreview = useRef();
+  const secondaryPreview = useRef();
   const [videoDevices, setVideoDevices] = useState([]);
+  const [isStreamActive, setIsStreamActive] = useState(false);
   const [audioDevices, setAudioDevices] = useState([]);
   const [selectedVideoDeviceId, setSelectedVideoDeviceId] = useState('');
   const [selectedAudioDeviceId, setSelectedAudioDeviceId] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [activeTab, setActiveTab] = useState('general');
+  const [activeSettingsTab, setActiveSettingsTab] = useState('stream');
+  const [globalError, setGlobalError] = useState('');
+
+  const [videoError, setVideoError] = useState(null);
+  const [audioError, setAudioError] = useState(null);
 
   useEffect(() => {
+    console.log(ingestServer)
     client.current = IVSBroadcastClient.create({
       // Enter the desired stream configuration
       streamConfig: IVSBroadcastClient.BASIC_LANDSCAPE,
@@ -29,31 +37,184 @@ function App() {
       ingestEndpoint: ingestServer,
     });
     window.client = client.current;
-    client.current.attachPreview(mainPreview.current);
   }, [])
 
-  const handleRequestPermission = async (e) => {
-    e.preventDefault();
+  const handleStreamStart = async (e) => {
     try {
       const devices = await requestPermissionAndGetDevices();
-      setVideoDevices(devices.videoDevices);
-      setAudioDevices(devices.audioDevices);
-      setErrorMessage('');
+      const { video, audio } = devices;
+      const { videoDevices, videoError: videoPermissionError } = video;
+      const { audioDevices, audioError: audioPermissionError } = audio;
+      setVideoDevices(videoDevices);
+      setAudioDevices(audioDevices);
+      setVideoError(videoPermissionError);
+      setAudioError(audioPermissionError);
+
+      if (videoPermissionError === null) {
+        let videoDeviceId;
+        if (selectedVideoDeviceId) {
+          videoDeviceId = selectedVideoDeviceId;
+        } else {
+          let savedDeviceId = getDevice('video');
+          if (savedDeviceId) {
+            const exists = videoDevices.find(device => device.deviceId === savedDeviceId);
+            console.log(exists)
+            if (!exists) {
+              savedDeviceId = null;
+              removeDevice('video');
+            }
+          }
+          videoDeviceId = savedDeviceId || videoDevices[0].deviceId;
+          setSelectedVideoDeviceId(videoDeviceId);
+        }
+        window.cameraStream = await navigator.mediaDevices.getUserMedia({
+          video: {
+            deviceId: videoDeviceId,
+            width: {
+              ideal: streamConfig.maxResolution.width,
+              max: streamConfig.maxResolution.width,
+            },
+            height: {
+              ideal: streamConfig.maxResolution.height,
+              max: streamConfig.maxResolution.height,
+            },
+          },
+        });
+        try {
+          client.current.removeVideoInputDevice('camera1');
+        } catch (e) { }
+        client.current.addVideoInputDevice(window.cameraStream, 'camera1', { index: 0 }); // only 'index' is required for the position parameter
+      }
+
+      if (audioPermissionError === null) {
+        let audioDeviceId;
+        if (selectedAudioDeviceId) {
+          audioDeviceId = selectedAudioDeviceId;
+        } else {
+          let savedDeviceId = getDevice('audio');
+          if (savedDeviceId) {
+            const exists = audioDevices.find(device => device.deviceId === savedDeviceId);
+            if (!exists) {
+              savedDeviceId = null;
+              removeDevice('audio');
+            }
+          }
+          audioDeviceId = savedDeviceId || audioDevices[0].deviceId;
+          setSelectedAudioDeviceId(audioDeviceId);
+        }
+
+        window.microphoneStream = await navigator.mediaDevices.getUserMedia({
+          audio: { deviceId: audioDeviceId },
+        });
+        try {
+          client.current.removeAudioInputDevice('mic1');
+        } catch (e) { }
+        client.current.addAudioInputDevice(window.microphoneStream, 'mic1');
+      }
+
+
+      if (audioPermissionError && videoPermissionError) {
+        const errorMsg = `Video: ${getPermissionErrorMessage(videoPermissionError)}, Audio: ${getPermissionErrorMessage(audioPermissionError)}`
+        setGlobalError(errorMsg)
+        return;
+      }
+
+      client.current.detachPreview(secondaryPreview.current);
+      client.current.attachPreview(mainPreview.current);
+      await client.current.startBroadcast(streamKey);
+      setIsStreamActive(true);
+
     } catch (error) {
-      handlePermissionError(error);
+      console.dir(error)
+    }
+
+  }
+
+  const handleStreamEnd = async () => {
+    await client.current.stopBroadcast(streamKey);
+    client.current.detachPreview(mainPreview.current);
+    setIsStreamActive(false)
+    // window.cameraStream = null
+  }
+
+  const initDevices = async () => {
+    try {
+      const devices = await requestPermissionAndGetDevices();
+      const { video, audio } = devices;
+      const { videoDevices, videoError: videoPermissionError } = video;
+      const { audioDevices, audioError: audioPermissionError } = audio;
+      setVideoDevices(videoDevices);
+      setAudioDevices(audioDevices);
+      setVideoError(videoError);
+      setAudioError(audioError);
+      if (videoPermissionError === null) {
+        let videoDeviceId;
+        if (selectedVideoDeviceId) {
+          videoDeviceId = selectedVideoDeviceId;
+        } else {
+          let savedDeviceId = getDevice('video');
+          if (savedDeviceId) {
+            const exists = videoDevices.find(device => device.deviceId === savedDeviceId);
+            console.log(exists)
+            if (!exists) {
+              savedDeviceId = null;
+              removeDevice('video');
+            }
+          }
+          videoDeviceId = savedDeviceId || videoDevices[0].deviceId;
+          setSelectedVideoDeviceId(videoDeviceId);
+        }
+        window.cameraStream = await navigator.mediaDevices.getUserMedia({
+          video: {
+            deviceId: videoDeviceId,
+            width: {
+              ideal: streamConfig.maxResolution.width,
+              max: streamConfig.maxResolution.width,
+            },
+            height: {
+              ideal: streamConfig.maxResolution.height,
+              max: streamConfig.maxResolution.height,
+            },
+          },
+        });
+        secondaryPreview.current.srcObject = window.cameraStream;
+        secondaryPreview.current.onloadedmetadata = async function (e) {
+          await secondaryPreview.current.play();
+        };
+      }
+
+      if (audioPermissionError === null) {
+        let audioDeviceId;
+        if (selectedAudioDeviceId) {
+          audioDeviceId = selectedAudioDeviceId;
+        } else {
+          let savedDeviceId = getDevice('audio');
+          if (savedDeviceId) {
+            const exists = audioDevices.find(device => device.deviceId === savedDeviceId);
+            if (!exists) {
+              savedDeviceId = null;
+              removeDevice('audio');
+            }
+          }
+          audioDeviceId = savedDeviceId || audioDevices[0].deviceId;
+          setSelectedAudioDeviceId(audioDeviceId);
+        }
+      }
+
+    } catch (error) {
+      console.dir(error)
     }
   }
 
-  const handlePermissionError = (error) => {
-    console.dir(error);
+  const getPermissionErrorMessage = (error) => {
     const { name } = error;
     let message;
-    switch(name) {
+    switch (name) {
       case 'NotAllowedError': {
         message = 'Permission denied by user or user agent';
         break;
       }
-      case 'OverconstrainedError': 
+      case 'OverconstrainedError':
       case 'NotFoundError': {
         message = 'There is no available devices';
         break;
@@ -62,13 +223,27 @@ function App() {
         message = 'Unknown error';
       }
     }
-    setErrorMessage(message);
+    return message;
+  }
+
+  const saveDevice = (type, deviceId) => {
+    localStorage.setItem(type, deviceId)
+  }
+
+  const getDevice = (type) => {
+    return localStorage.getItem(type)
+  }
+
+  const removeDevice = (type) => {
+    localStorage.removeItem(type)
   }
 
   const handleDeviceChange = async (type, deviceId) => {
+    saveDevice(type, deviceId);
     try {
       if (type === 'video') {
         setSelectedVideoDeviceId(deviceId);
+
         if (window.cameraStream) {
           for (const track of window.cameraStream.getTracks()) {
             track.stop();
@@ -87,13 +262,22 @@ function App() {
             },
           },
         });
-        try {
-          client.current.removeVideoInputDevice(deviceId);
-        } catch (e) { }
-        client.current.addVideoInputDevice(window.cameraStream, deviceId, { index: 0 }); // only 'index' is required for the position parameter
+
+        if (isStreamActive) {
+          try {
+            client.current.removeVideoInputDevice('camera1');
+          } catch (e) { }
+          client.current.addVideoInputDevice(window.cameraStream, 'camera1', { index: 0 }); // only 'index' is required for the position parameter
+        } 
+
+        secondaryPreview.current.srcObject = window.cameraStream;
+        secondaryPreview.current.onloadedmetadata = async function (e) {
+          await secondaryPreview.current.play();
+        };
+          // client.current.attachPreview(secondaryPreview.current)
       } else {
         try {
-          client.current.removeAudioInputDevice(deviceId);
+          client.current.removeAudioInputDevice('mic1');
         } catch (e) { }
         if (window.microphoneStream) {
           for (const track of window.microphoneStream.getTracks()) {
@@ -103,11 +287,11 @@ function App() {
         window.microphoneStream = await navigator.mediaDevices.getUserMedia({
           audio: { deviceId },
         });
-        client.current.addAudioInputDevice(window.microphoneStream, deviceId);
+        client.current.addAudioInputDevice(window.microphoneStream, 'mic1');
         setSelectedAudioDeviceId(deviceId);
       }
     } catch (error) {
-      handlePermissionError(error)
+      console.dir(error)
     }
   }
 
@@ -115,59 +299,136 @@ function App() {
     <div className='grid grid-cols-2'>
       <div className='outline outline-1 outline-red-900 flex flex-col'>
         <canvas className='bg-black' id="mainPreview" ref={mainPreview}></canvas>
+        <span className='text-red-500'>{globalError}</span>
         <div className='flex justify-center mt-2'>
           <button
             className="text-center text-indigo-400 font-bold rounded py-2 w-2/12 focus:outline-none bg-gray-900 border-2 border-indigo-400"
-            onClick={handleRequestPermission}
+            onClick={(e) => {
+              e.preventDefault()
+              if (isStreamActive) {
+                handleStreamEnd();
+              } else {
+                handleStreamStart()
+              }
+            }}
           >
-            Request Permission
+            {isStreamActive ? "End Stream" : 'Start Stream'}
           </button>
         </div>
       </div>
-      <div className='outline outline-1 outline-yellow-900 px-4'>
-        <div>
-          <div className="">
-            <div>
-              <span>Video devices:</span>
-              <form>
-                <select
-                  onChange={(event) => handleDeviceChange('video', event.target.value)}
-                  value={selectedVideoDeviceId}
-                >
-                  <option value="" disabled>Select device</option>
-                  {
-                    videoDevices.map((device, idx) => {
-                      return (
-                        <option key={idx} value={device.deviceId}>{device.label}</option>
-                      )
-                    })
-                  }
-                </select>
-              </form>
-            </div>
-            <div className='mt-5'>
-              <span>Audio devices:</span>
-              <form>
-                <select
-                  onChange={(event) => handleDeviceChange('audio', event.target.value)}
-                  value={selectedAudioDeviceId}
-                >
-                  <option value="" disabled>Select device</option>
-                  {
-                    audioDevices.map((device, idx) => {
-                      return (
-                        <option key={idx} value={device.deviceId}>{device.label}</option>
-                      )
-                    })
-                  }
-                </select>
-              </form>
-            </div>
-            <div>
-              <span className='text-red-600 font-extrabold'>{errorMessage}</span>
-            </div>
+      <div className='flex flex-col p-4'>
+        <div className='flex w-full'>
+          <div
+            onClick={() => { setActiveTab('general') }}
+            className={`flex w-1/2  cursor-pointer p-4 rounded-lg ${'general' === activeTab ? 'text-sky-900 bg-violet-200' : ''}`}>
+            General
+          </div>
+          <div
+            onClick={() => { setActiveTab('settings'); initDevices() }}
+            className={`flex w-1/2 cursor-pointer  p-4 rounded-lg ${'settings' === activeTab ? 'text-sky-900 bg-violet-200' : ''}`}>
+            Settings
           </div>
         </div>
+        {
+          'general' === activeTab && (
+            <div>
+              ...
+            </div>
+          )
+        }
+        {
+          'settings' === activeTab && (
+            <div className='outline outline-1 p-4 mt-4'>
+              <div className='flex w-1/2 mx-auto'>
+                <div
+                  onClick={() => { setActiveSettingsTab('stream') }}
+                  className={`flex w-1/2  cursor-pointer p-4 ${'stream' === activeSettingsTab ? 'text-sky-900 bg-violet-200' : ''} rounded-lg`}>
+                  Stream
+                </div>
+                <div
+                  onClick={() => { setActiveSettingsTab('other') }}
+                  className={`flex w-1/2 cursor-pointer  p-4 ${'other' === activeSettingsTab ? 'text-sky-900 bg-violet-200' : ''} rounded-lg`}>
+                  Other
+                </div>
+              </div>
+              {
+                'other' === activeSettingsTab && (
+                  <div>
+                    ...
+                  </div>
+                )
+              }
+              {
+                'stream' === activeSettingsTab && (
+                  <div className='mt-3'>
+                    <div className="">
+                      <div>
+                        <span>Video devices:</span>
+                        <form>
+                          <select
+                            onChange={(event) => handleDeviceChange('video', event.target.value)}
+                            value={selectedVideoDeviceId}
+                          >
+                            <option value="" disabled>Select device</option>
+                            {
+                              videoDevices.map((device, idx) => {
+                                return (
+                                  <option key={idx} value={device.deviceId}>{device.label}</option>
+                                )
+                              })
+                            }
+                          </select>
+                        </form>
+                        {
+                          videoError && (
+                            <span className='text-red-600'>
+                              {getPermissionErrorMessage(videoError)}
+                            </span>
+                          )
+                        }
+                        {
+                          true && (
+                            <div className='w-60 mt-4 bg-black'>
+                              {/* <canvas className='bg-black' id="secondaryPreview" ref={secondaryPreview}></canvas> */}
+                              <video autoPlay={true} muted={true} id='videoElement' ref={secondaryPreview}></video>
+                            </div>
+
+                          )
+                        }
+                      </div>
+                      <div className='mt-5'>
+                        <span>Audio devices:</span>
+                        <form>
+                          <select
+                            onChange={(event) => handleDeviceChange('audio', event.target.value)}
+                            value={selectedAudioDeviceId}
+                          >
+                            <option value="" disabled>Select device</option>
+                            {
+                              audioDevices.map((device, idx) => {
+                                return (
+                                  <option key={idx} value={device.deviceId}>{device.label}</option>
+                                )
+                              })
+                            }
+                          </select>
+                        </form>
+                        {
+                          audioError && (
+                            <span className='text-red-600'>
+                              {getPermissionErrorMessage(audioError)}
+                            </span>
+                          )
+                        }
+                      </div>
+                    </div>
+                  </div>
+                )
+              }
+            </div>
+          )
+        }
+
       </div>
     </div>
   );
--- src/helpers.js
+++ src/helpers.js
@@ -1,15 +1,31 @@
 async function requestPermissionAndGetDevices() {
+    let videoDevices = [];
+    let audioDevices = [];
+    let videoError = null;
+    let audioError = null;
     try {
-        await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        await navigator.mediaDevices.getUserMedia({ video: true });
         const devices = await navigator.mediaDevices.enumerateDevices();
-        const videoDevices = devices.filter((d) => d.kind === 'videoinput');
-        const audioDevices = devices.filter((d) => d.kind === 'audioinput');
-        return {
-            videoDevices,
-            audioDevices
-        }
+        videoDevices = devices.filter((d) => d.kind === 'videoinput');
     } catch (err) {
-        throw err;
+        videoError = err;
+    }
+    try {
+        await navigator.mediaDevices.getUserMedia({ audio: true });
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        audioDevices = devices.filter((d) => d.kind === 'audioinput');
+    } catch (err) {
+        audioError = err;
+    }
+    return {
+        video: {
+            videoDevices,
+            videoError,
+        },
+        audio: {
+            audioDevices,
+            audioError,
+        }
     }
 }
 
